db85a2666762ef559e846e358142be122cfa1b31
Mayur Barpande <mkbarpande@gmail.com>
2022-01-27 19:54:09 +0530

    Generic Extends

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { extendInterface, extendType, genericArray, genericType, narrowing, play } from "../playground";
+import { extendInterface, extendType, genericArray, genericExtends, genericType, narrowing, play } from "../playground";
 
 export default function Home() {
   useEffect(() => {
@@ -9,6 +9,7 @@ export default function Home() {
     extendType(); // example of extending type
     genericArray(); // example of generic array
     genericType(); // example of generic type
+    genericExtends(); // example of generic extends
   }, []);
   return <div>He</div>;
 }
--- playground.ts
+++ playground.ts
@@ -230,5 +230,52 @@ export const genericType = () => {
   logger2.log(numbers, (num) => {
     console.log(num);
   });
+};
 
+/* ************* GENERIC EXTENDS **************** */
+
+export const genericExtends = () => {
+  interface Person {
+    name: string;
+    age: number;
+    city: string;
+  }
+
+  class Student implements Person {
+    name = "";
+    age = 0;
+    city = "";
+  }
+
+  class BussinessPerson implements Person {
+    name = "";
+    age = 0;
+    city = "";
+    salary = 1000;
+  }
+
+  type Car = {
+    brand: string;
+    age: number;
+    name: string;
+    city: string;
+  };
+
+  class Logger<T extends Person> {
+    log(items: Array<T>, callback: (i: T) => void) {
+      items.forEach((items) => {
+        callback(items);
+      });
+    }
+  }
+
+  const logger = new Logger();
+
+  const persons = [
+    { name: "filip", age: 30, city: "", brand: "" },
+    { name: "john", age: 25, city: "", brand: "" },
+  ];
+  logger.log(persons, (person) => {
+    console.log(person);
+  });
 };
