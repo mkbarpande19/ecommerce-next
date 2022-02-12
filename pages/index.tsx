1afab44bf146e3b134057fe5d680af75c9c0cf15
Mayur Barpande <mkbarpande@gmail.com>
2022-02-12 17:00:33 +0530

    Infer Get All Static props and get all products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import { useEffect } from "react";
-import { extendInterface, extendType, genericArray, genericExtends, genericType, genericTypesWithExtends, narrowing, play } from "../playground";
+import { InferGetStaticPropsType } from "next";
+import React from "react";
 
-export default function Home() {
-  useEffect(() => {
-    play(); //simple function
-    narrowing(); // Narrowing function with conditional statements also we can use switch cases
-    extendInterface(); // example of extending interface for the same type of data.
-    extendType(); // example of extending type
-    genericArray(); // example of generic array
-    genericType(); // example of generic type
-    genericExtends(); // example of generic extends
-    genericTypesWithExtends(); // example of generic types with extends
-  }, []);
-  return <div>He</div>;
+import getAllProducts from "../framework/shopify/product/get-all-products";
+
+export async function getStaticProps() {
+  const products = await getAllProducts();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      <span>{products} </span>
+    </div>
+  );
 }
