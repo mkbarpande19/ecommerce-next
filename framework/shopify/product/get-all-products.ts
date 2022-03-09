14c8e89a4a51ba300d0307fb6ef9b3f2dc8b7735
Mayur Barpande <mkbarpande@gmail.com>
2022-03-09 16:52:26 +0530

    Specify Fetcher Return Type

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,11 +1,14 @@
 // use of fetch to get data from api
 import fetchApi from "../utils/fetch-api";
 import getAllProductsQuery from "../utils/queries/get-all-products";
+import { ProductConnection } from "../schema";
 
-
-const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi({ query: getAllProductsQuery });
-  return products.data;
+type ReturnType = {
+  products: ProductConnection;
+};
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
+  return data.products;
 };
 
 export default getAllProducts;
