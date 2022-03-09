c12c3cfe4ad4ec84c02ae599c4c4e87c5c2a9de5
Mayur Barpande <mkbarpande@gmail.com>
2022-03-09 17:13:02 +0530

    normalize products

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -2,6 +2,7 @@
 import fetchApi from "../utils/fetch-api";
 import getAllProductsQuery from "../utils/queries/get-all-products";
 import { ProductConnection } from "../schema";
+import { normalizeProduct } from "../utils/normalize";
 
 type ReturnType = {
   products: ProductConnection;
@@ -10,9 +11,9 @@ const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
 
   const products =
-    data.products.edges.map(({ node: products }) => {
-      return products;
-    }) ?? [];
+    data.products.edges.map(({ node: products }) =>
+      normalizeProduct(products)
+    ) ?? [];
   return products;
 };
 
---
+++ framework/shopify/utils/normalize.ts
@@ -0,0 +1,25 @@
+// Normalize products
+
+import { Product as ShopifyProduct } from "../schema";
+
+export function normalizeProduct(productNode: ShopifyProduct): any {
+    const { 
+        id, 
+        title: name, 
+        handle, 
+        vendor, 
+        description, 
+        ...rest 
+    } = productNode;
+
+    const product = {
+        id,
+        name,
+        vendor,
+        description,
+        path:`/${handle}`,
+        slug: handle.replace(/^\+|\/+$/g,""),
+        ...rest 
+    }
+    return product
+}
